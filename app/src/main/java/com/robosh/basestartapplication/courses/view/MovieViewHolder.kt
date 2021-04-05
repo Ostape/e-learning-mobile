e13d2b99fd49a282f4e29118fe862469127d7302
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-04-05 23:07:04 +0300

    add card view

--- app/src/main/java/com/robosh/basestartapplication/courses/view/MovieViewHolder.kt
+++ app/src/main/java/com/robosh/basestartapplication/courses/view/MovieViewHolder.kt
@@ -5,7 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.robosh.basestartapplication.R
-import com.robosh.basestartapplication.databinding.ViewHolderMovieBinding
+import com.robosh.basestartapplication.databinding.ViewHolderCourseBinding
 import com.robosh.basestartapplication.model.Movie
 import com.robosh.basestartapplication.net.api.MovieDbApi.Companion.IMAGE_BASE_URL
 import com.squareup.picasso.Picasso
@@ -22,13 +22,13 @@ class MovieViewHolder private constructor(
         ): MovieViewHolder {
             return MovieViewHolder(
                 LayoutInflater.from(parent.context)
-                    .inflate(R.layout.view_holder_movie, parent, false),
+                    .inflate(R.layout.view_holder_course, parent, false),
                 clickListener
             )
         }
     }
 
-    private val binding: ViewHolderMovieBinding = ViewHolderMovieBinding.bind(view)
+    private val binding: ViewHolderCourseBinding = ViewHolderCourseBinding.bind(view)
 
     fun bind(movie: Movie) {
         with(binding) {
