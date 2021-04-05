3862c4b347a974aac48123b03b94d89f509f1a3d
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-04-05 16:15:26 +0300

    add bottom navigation actions

--- app/src/main/java/com/robosh/basestartapplication/BrowseActivity.kt
+++ app/src/main/java/com/robosh/basestartapplication/BrowseActivity.kt
@@ -2,12 +2,26 @@ package com.robosh.basestartapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_browse.*
 
 @AndroidEntryPoint
 class BrowseActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browse)
+        setupViews()
+    }
+
+    fun setupViews() {
+        // Finding the Navigation Controller
+        var navController = findNavController(R.id.distanceNavHostFragment)
+
+        // Setting Navigation Controller with the BottomNavigationView
+        bottomNavView.setupWithNavController(navController)
     }
 }
