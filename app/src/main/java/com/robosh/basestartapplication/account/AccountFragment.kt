df0dc3e33c026f6eb287ff44ff889a47f8234e3d
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-04-06 22:38:59 +0300

    add alert dialog

--- app/src/main/java/com/robosh/basestartapplication/account/AccountFragment.kt
+++ app/src/main/java/com/robosh/basestartapplication/account/AccountFragment.kt
@@ -1,5 +1,6 @@
 package com.robosh.basestartapplication.account
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.robosh.basestartapplication.R
 import com.robosh.basestartapplication.databinding.FragmentAccountBinding
+
 
 class AccountFragment : Fragment() {
 
@@ -34,6 +36,18 @@ class AccountFragment : Fragment() {
         }
         binding.certificateId.setOnClickListener {
             findNavController().navigate(R.id.action_accountFragment_to_certificateFragment)
+        }
+        binding.logoutId.setOnClickListener {
+            AlertDialog.Builder(context)
+                .setTitle("Вийти з акаунту?")
+                .setMessage("Ви впевненні що хочете вийти з аккаунту?")
+                // The dialog is automatically dismissed when a dialog button is clicked.
+                .setPositiveButton("Вийти", { dialog, which ->
+                    // Continue with delete operation
+                })
+                .setNegativeButton("Відмінити", null)
+                .setIcon(R.drawable.ic_log_out)
+                .show()
         }
     }
 }
