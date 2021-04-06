055867ca179f2dcd475effb727cd133bade31be5
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-04-06 14:53:02 +0300

    add transition to login fragment

--- app/src/main/java/com/robosh/basestartapplication/account/AccountFragment.kt
+++ app/src/main/java/com/robosh/basestartapplication/account/AccountFragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.robosh.basestartapplication.R
 import com.robosh.basestartapplication.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment() {
 
     private lateinit var binding: FragmentAccountBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        findNavController().navigate(R.id.action_accountFragment_to_loginFragment)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
