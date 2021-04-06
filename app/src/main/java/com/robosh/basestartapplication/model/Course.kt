5b8d2cd3837770b4cb66513cf287ce4f68cfc767
Nikita Bortnichuk <Nikita_Bortnichuk@epam.com>
2021-04-07 01:23:01 +0300

    add mock data and icon

--- app/src/main/java/com/robosh/basestartapplication/model/Course.kt
+++ app/src/main/java/com/robosh/basestartapplication/model/Course.kt
@@ -1,7 +1,5 @@
 package com.robosh.basestartapplication.model
 
-import java.util.*
-
 data class Course(
     val id: String,
     val name: String,
@@ -11,5 +9,5 @@ data class Course(
     val teachers: List<Teacher>?,
     val lessons: List<Lesson>,
     val comments: List<String>?,
-    val deadline: Date?
+    val deadline: String?
 )
